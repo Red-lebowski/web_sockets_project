6581965179d48ffa8308c270cb8fe52a7c036b35
Red <red@Reds-MacBook-Air.local>
2020-05-02 12:38:01 +1000

    cleaned up frontend websocket implementation

--- frontend/src/CurrentSecond.tsx
+++ frontend/src/CurrentSecond.tsx
@@ -1,79 +1,77 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, FormEvent } from 'react'
 
-const nowWebSocket = new WebSocket(`ws://${process.env.REACT_APP_SERVER_URL}/now-updated`)
-const isOddWebSocket = new WebSocket(`ws://${process.env.REACT_APP_SERVER_URL}/is-odd`)
+const serverURL = process.env.REACT_APP_SERVER_URL
+const nowWebSocket = new WebSocket(`ws://${serverURL}/now-updated`)
+const isOddWebSocket = new WebSocket(`ws://${serverURL}/is-odd`)
 
-async function getCurrentSecond(){
-    const currentSecond: CurrentSecondResponse = await fetch(`http://${process.env.REACT_APP_SERVER_URL}/now`)
-                                        .then(res => res.json())
-                                        .catch(console.error)
-    return currentSecond?.time || 'server error'
+export default function CurrentSecond() {
+  const [numberInput, setNumberInput] = useState<number>(0)
+  const [numbers, setNumbers] = useState<Array<IsOddObject>>([])
+  const [currentSecond, setCurrentSecond] = useState<String>('connecting...')
+  const [canSubmitNumber, setCanSubmitNumber] = useState<Boolean>(false)
+
+  useEffect(() => {
+
+    nowWebSocket.onmessage = function (event: any) {
+      setCurrentSecond(event.data)
+    }
+
+    isOddWebSocket.onopen = () => setCanSubmitNumber(true)
+
+    isOddWebSocket.onmessage = function (event) {
+      const result: IsOddObject = JSON.parse(event.data)
+      const newNumbers = numbers
+      newNumbers.push(result)
+      setNumbers(newNumbers)
+    }
+
+  }, [nowWebSocket.readyState, isOddWebSocket.readyState])
+
+  const sendNumber = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const number = numberInput
+    isOddWebSocket.send(JSON.stringify({ number }))
+  }
+
+  const isOddStyle = canSubmitNumber ? 
+                      {}
+                      : {filter: 'blur(1px)', pointEvents: 'none'}
+
+  return (
+    <div className="current-second-container">
+      <div className="current-second">
+        The current second is: {currentSecond}
+      </div>
+
+      <div className="is-odd-calc" style={isOddStyle}>
+        <form className="is-odd-form" onSubmit={sendNumber}>
+          <input type='number' onChange={e => setNumberInput(Number(e.target.value))} />
+          <input type='submit' value='is odd'/>
+        </form>
+
+        <div className="is-odd-results">
+          {numbers.map(function (item: IsOddObject, id: number) {
+            return (<li key={id}>{item.number}|{item.is_odd}</li>)
+          })}
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
-function send(webSocket:any, message:any, callback:any) {
-    waitForConnection(webSocket, function () {
-        webSocket.send(message);
-        if (typeof callback !== 'undefined') {
-          callback();
-        }
-    }, 1000);
-};
-
-function waitForConnection(webSocket:any, callback:any, interval:number) {
-    if (webSocket.readyState === 1) {
-        callback();
-    } else {
-        // optional: implement backoff for interval here
-        setTimeout(function () {
-            waitForConnection(webSocket, callback, interval);
-        }, interval);
-    }
-};
-
-
-export default function CurrentSecond(){
-    const [currentSecond, setCurrentSecond] = useState<String>('none')
-    const [numbers, setNumbers] = useState<any>([])
-    const [numberInput, setNumberInput] = useState<number>(0)
-
-    useEffect(() => {
-        nowWebSocket.onmessage = function(event: any) {
-            setCurrentSecond(event.data)
-        }
-        // TODO: defint the structure of the websocket event in a type
-        isOddWebSocket.onmessage = function(event){
-            const result: IsOddObject = JSON.parse(event.data)
-            const newNumbers = numbers
-            newNumbers.push(result)
-            setNumbers(newNumbers)
-        }
-        send(isOddWebSocket, JSON.stringify({number: 20}), console.log)
-    }, [])
-
-    const sendNumber = (number: Number) => {
-        console.log('go')
-        send(isOddWebSocket, JSON.stringify({number}), console.log)
-    }
-
-    return (
-        <div className="">
-            The current second is: {currentSecond}
-            <div className="">
-                <input type='number' onChange={e => setNumberInput(Number(e.target.value))}/>
-                <input type='submit' value='is odd' onClick={() => sendNumber(numberInput)}/>
-                {numbers.map(function(item:IsOddObject, id:number){
-                    return (<li key={id}>{item.number}|{item.is_odd}</li>)
-                })}
-            </div>
-        </div>
-    )
+enum SocketState {
+  Connecting = 0,
+  Open = 1,
+  Closing = 2,
+  Closed = 3,
 }
 
 interface CurrentSecondResponse {
-    time: String
+  time: String
 }
 
 interface IsOddObject {
-    number: number
-    is_odd: string
+  number: number
+  is_odd: string
 }
